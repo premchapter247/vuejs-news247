0e06147ff0d3ea1993459b400b49464f383cb41f
ankitChapter247 <ankit.singh@chapter247.com>
2021-06-03 19:12:37 +0530

    work on news with image slider section

--- src/views/front/component/LatestNews.vue
+++ src/views/front/component/LatestNews.vue
@@ -1,35 +1,54 @@
 <template>
-  <div>
-      <VueSlickCarousel
-        ref="c1"
-        :asNavFor="$refs.c2"
-        :focusOnSelect="true">
-        <div>
-            <div v-for="item in list"
-                :key="item.id">
-                <h3>{{ item.title }}</h3>
-            </div> 
-        </div>
+    <v-container>
+        <v-row>
+            <v-col cols="8">
+                <div>
+                    <VueSlickCarousel
+                        :arrows="true" 
+                        ref="c1"
+                        :asNavFor="$refs.c2"
+                        :focusOnSelect="true">
+                    
+                            <div v-for="item in list"
+                                :key="item.id">
+                                <v-img                            
+                                src="https://picsum.photos/id/11/500/300"
+                                ></v-img>
+                            </div> 
+                        
 
-        </VueSlickCarousel>
-        <VueSlickCarousel
-        ref="c2"
-        :asNavFor="$refs.c1"
-        :slidesToShow="4"
-        :focusOnSelect="true">
-        <div>
-            <div v-for="item in list"
-                :key="item.id">
-                <h3>{{ item.title }}</h3>
-            </div>    
-        </div>
-        </VueSlickCarousel>
-  </div>
+                        </VueSlickCarousel>
+                        <VueSlickCarousel
+                        :arrows="true" 
+                        ref="c2"
+                        :asNavFor="$refs.c1"
+                        :slidesToShow="4"
+                        :focusOnSelect="true">
+                    
+                            <div v-for="item in list"
+                                :key="item.id">
+                                <v-img
+                                max-height="150"
+                                max-width="250"
+                                src="https://picsum.photos/id/11/500/300"
+                                ></v-img>                        
+                            </div>    
+                    
+                        </VueSlickCarousel>
+                </div>
+            </v-col>
+            <v-col cols="4">
+                <div>
+                    <p>tabbing section</p>
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 <script>
   import VueSlickCarousel from 'vue-slick-carousel'
   import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-  /* import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css' */
+  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
   import axios from 'axios'
 
@@ -37,13 +56,15 @@
     name: 'LatestNews',
     components: { VueSlickCarousel },
     data () {
-      return { list: undefined }
+      return { 
+          list: 'undefined',
+          }
     },
     mounted () {
-      axios.get('http://127.0.0.1:8000/api/category/index')
+      axios.post('http://127.0.0.1:8000/api/post/list', { start: 0, total: 10 })
         .then((resp) => {
-          this.list = resp.data.data.data
-          console.warn(resp.data.data.data)
+          this.list = resp.data.data
+          console.warn(resp.data.data)
         })
     },
 
