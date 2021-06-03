858509a9417deb74cdd0400f9c95dca63cdb865a
ankitChapter247 <ankit.singh@chapter247.com>
2021-06-03 15:31:59 +0530

    latest news section

--- src/views/front/component/LatestNews.vue
+++ src/views/front/component/LatestNews.vue
@@ -1,20 +1,29 @@
 <template>
   <div>
       <VueSlickCarousel
-  ref="c1"
-  :asNavFor="$refs.c2"
-  :focusOnSelect="true">
-  <div><h3>1</h3></div>
-  /*...*/
-</VueSlickCarousel>
-    <VueSlickCarousel
-  ref="c2"
-  :asNavFor="$refs.c1"
-  :slidesToShow="4"
-  :focusOnSelect="true">
-  <div><h3>1</h3></div>
-  /*...*/
-</VueSlickCarousel>
+        ref="c1"
+        :asNavFor="$refs.c2"
+        :focusOnSelect="true">
+        <div>
+            <div v-for="item in list"
+                :key="item.id">
+                <h3>{{ item.title }}</h3>
+            </div> 
+        </div>
+
+        </VueSlickCarousel>
+        <VueSlickCarousel
+        ref="c2"
+        :asNavFor="$refs.c1"
+        :slidesToShow="4"
+        :focusOnSelect="true">
+        <div>
+            <div v-for="item in list"
+                :key="item.id">
+                <h3>{{ item.title }}</h3>
+            </div>    
+        </div>
+        </VueSlickCarousel>
   </div>
 </template>
 <script>
@@ -22,8 +31,21 @@
   // optional style for arrows & dots
   import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
+  import axios from 'axios'
+
   export default {
     name: 'LatestNews',
     components: { VueSlickCarousel },
+    data () {
+      return { list: undefined }
+    },
+    mounted () {
+      axios.get('http://127.0.0.1:8000/api/category/index')
+        .then((resp) => {
+          this.list = resp.data.data.data
+          console.warn(resp.data.data.data)
+        })
+    },
+
   }
 </script>
