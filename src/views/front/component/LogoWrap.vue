9731ec585bfea7c8d1dc0f701463b48cea980c55
Rohit-chapter <rohit.t@chapter247.com>
2021-06-04 13:21:53 +0530

    MenuBar design

--- src/views/front/component/LogoWrap.vue
+++ src/views/front/component/LogoWrap.vue
@@ -1,10 +1,25 @@
 <template>  
-    <v-container>
-        <div>Logo</div>
-    </v-container>
+    <section class="logo-wrap white_bg">
+        <v-container>
+            <a href="" class="logo">
+                <v-img
+                 src="https://jkcs.000webhostapp.com/news247.png"
+                ></v-img>
+            </a>
+        </v-container>
+    </section>
 </template>
 <script>
 export default {
     name: 'LogoBar',
 }
 </script>
+<style scoped>
+.logo{
+    max-width: 250px;
+    display: block;
+}
+.logo-wrap{
+    padding: 20px 0;
+}
+</style>
--- src/views/front/component/Menu.vue
+++ src/views/front/component/Menu.vue
@@ -1,3 +1,151 @@
 <template>
-    
+    <section class="nav-wrap white_bg">
+        <v-container>
+            <nav class="navbar">
+                <ul class="nav-ul">
+                    <li class="nav-li">
+                        <a href="" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-li has-sub">
+                        <a href="" class="nav-link">Pages</a>
+                        <ul class="sub-menu-ul">
+                            <li class="sub-menu-li">
+                                <a href="" class="sub-menu-link">Pages 1</a>
+                            </li>
+                            <li class="sub-menu-li">
+                                <a href="" class="sub-menu-link">Pages 2</a>
+                            </li>
+                            <li class="sub-menu-li">
+                                <a href="" class="sub-menu-link">Pages 3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-li">
+                        <a href="" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-li has-sub">
+                        <a href="" class="nav-link">Pages</a>
+                        <ul class="sub-menu-ul">
+                            <li class="sub-menu-li">
+                                <a href="" class="sub-menu-link">Pages 1</a>
+                            </li>
+                            <li class="sub-menu-li">
+                                <a href="" class="sub-menu-link">Pages 2</a>
+                            </li>
+                            <li class="sub-menu-li">
+                                <a href="" class="sub-menu-link">Pages 3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-li">
+                        <a href="" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-li">
+                        <a href="" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-li">
+                        <a href="" class="nav-link">Home</a>
+                    </li>
+                </ul>
+            </nav>
+        </v-container>
+    </section>
 </template>
+<script>
+export default {
+    name: 'MenuBar' 
+}
+</script>
+<style scoped>
+.nav-ul{
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.nav-li{
+    position: relative;
+}
+.nav-link{
+    text-decoration: none;
+    padding: 0 15px;
+    display: block;
+    line-height: 60px;
+    color: #17222b;
+    font-size: 16px;
+    font-weight: 400;
+    text-transform: capitalize;
+    position: relative;
+    transition: .35s;
+}
+.sub-menu-ul{
+    list-style: none;
+    position: absolute;
+    text-align: left;
+    background: #fff;
+    z-index: 100;
+    min-width: 230px;
+    padding: 7px;
+    border-radius: 3px;
+    box-shadow: 0 0 15px rgb(0 0 0 / 6%);
+    border: none!important;
+    display: block;
+    opacity: 0;
+    transform: scaleY(0);
+    transform-origin: top;
+    visibility: hidden;
+    transition: all .3s;
+    transition-timing-function: cubic-bezier(.25,.1,0,.45);
+    margin: 0;
+}
+.nav-li:hover .sub-menu-ul{
+    opacity: 1;
+    visibility: visible;
+    transform: scaleY(1);
+}
+.sub-menu-link{
+    display: block;
+    font-size: inherit;
+    font-weight: 400;
+    line-height: normal;
+    text-decoration: none;
+    padding: 10px 25px;
+    color: #333;
+    background: #fff;
+    text-transform: capitalize;
+}
+.sub-menu-link:hover{
+    background: #1091ff;
+    color: #fff;
+}
+@media(min-width: 992px){
+    .nav-li:before{
+        position: absolute;
+        content: "";
+        height: 0;
+        width: 100%;
+        background: #f3f3f4;
+        left: 0;
+        top: 0;
+        transition: all .3s;
+    }
+    .nav-li:after{
+        position: absolute;
+        content: "";
+        height: 0;
+        width: 100%;
+        background: #1091ff;
+        bottom: 0;
+        left: 0;
+        z-index: 2;
+        transition: all .3s;
+    }
+    .nav-li:hover:before{
+        height: 100%;
+    }
+    .nav-li:hover:after{
+        height: 2px;
+    }
+}
+</style>
