b9aa406aaab49e8b1bee093f486e7f4ad934aa66
Vijay <vijay.v@chapter247.com>
2021-06-05 21:28:51 +0530

    Trandding News Section Completed

--- src/views/front/component/FeatureNews.vue
+++ src/views/front/component/FeatureNews.vue
@@ -1,97 +1,70 @@
 <template>
+  <div>
+    <div>
+      <h2 class="widget-title">Feature News</h2>
+    </div>
 
-  <!-- <div>
-    <h2>Trending News</h2>
-  </div> -->
+    <VueSlickCarousel :arrows="true" :dots="false" v-bind="setting">
+      <div>
+        <CardImage></CardImage>
+      </div>
 
-  <VueSlickCarousel :arrows="true" :dots="false" v-bind="setting">
-      <div>
-        <Card_1></Card_1>
-      </div>
-      <div>
-        <Card_1></Card_1>
-      </div>
-      <div>
-        <Card_1></Card_1>
-      </div>
-      <div>
-        <Card_1></Card_1>
-      </div>
-  </VueSlickCarousel>
+    </VueSlickCarousel>
+    <div class="border_black"></div>
+    <div class="space-30"></div>
+  </div>
 </template>
 
 <script>
-  import VueSlickCarousel from 'vue-slick-carousel'
-  import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+import CardImage from "./CardImage.vue";
 
-  // optional style for arrows & dots
-  import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
-  import Card_1 from './Card_1'
-
-  export default {
-    name: "MyComponent",
-    components: {
-      VueSlickCarousel,
-      Card_1
-    },
-    data() {
-      return {
-        setting: {
-
-          "dots": true,
-          "infinite": true,
-          "speed": 500,
-          "slidesToShow": 2,
-          "slidesToScroll": 2,
-          "initialSlide": 2,
-          "responsive": [{
-            "breakpoint": 1024,
-            "settings": {
-              "slidesToShow": 1,
-              "slidesToScroll": 1,
-              "infinite": true,
-              "dots": false
-            }
-          }, ]
-        }
-      };
-    },
-  };
-
+export default {
+  name: "MyComponent",
+  components: {
+    VueSlickCarousel,
+    CardImage,
+  },
+  data() {
+    return {
+      setting: {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        initialSlide: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              initialSlide: 1,
+            },
+          },
+        ],
+      },
+    };
+  },
+};
 </script>
 
-<!-- <template>
-      <v-row dense>
-        <v-col
-          v-for="card in cards"
-          :key="card.title"
-          :cols="card.flex"
-          :sm="card.sm"
-          :md="card.md"
-          :lg="card.lg"
-        >
-            <v-img
-              :src="card.src"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="255px"
-              width="320px"
-            >
-              <v-card-title v-text="card.title"></v-card-title>
-            </v-img>
-        </v-col>
-      </v-row>
-</template>
-<script>
-  export default {
-    data: () => ({
-      cards: [
-        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3, sm: 12, md: 6, lg: 4 },
-        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3, sm: 12, md: 6, lg: 4 },
-        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3, sm: 12, md: 6, lg: 4 },
-        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3, sm: 12, md: 6, lg: 4 },
-      ],
-    }),
-  }
-</script>
--->
+<style scoped>
+.widget-title {
+  font-size: 24px;
+  margin-bottom: 20px;
+  font-weight: 500;
+}
+</style>
