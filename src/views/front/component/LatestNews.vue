c6c2999d1d34f28e8afa468f6a252f8775d43fa4
Vijay <vijay.v@chapter247.com>
2021-06-11 15:49:17 +0530

    Use component inside components.

--- src/views/front/component/LatestNews.vue
+++ src/views/front/component/LatestNews.vue
@@ -13,19 +13,29 @@
                 :asNavFor="$refs.c2"
                 :focusOnSelect="true"
               >
-                <div v-for="item in list" :key="item.id" class="latest-news-carousel post-dark-overlay">
+                <div
+                  v-for="item in list"
+                  :key="item.id"
+                  class="latest-news-carousel post-dark-overlay"
+                >
                   <img
-                    :src="'http://127.0.0.1:8000/image/' + item.image" class="img-responsive" draggable="false"
-                  >
+                    :src="'http://127.0.0.1:8000/image/' + item.image"
+                    class="img-responsive"
+                    draggable="false"
+                  />
                   <div class="post-content-wrap">
                     <div class="post-card-metas">
-                      <a href="" class="post-card-category">{{ item.category.title}}</a>
-                      <a href="" class="post-card-date">{{ moment(item.created_at).format("MMMM D, YYYY") }} </a>
+                      <a href="" class="post-card-category">{{
+                        item.category.title
+                      }}</a>
+                      <a href="" class="post-card-date"
+                        >{{ moment(item.created_at).format("MMMM D, YYYY") }}
+                      </a>
                     </div>
                     <a href="" class="post-card-title">
-                      {{ item.title}}
+                      {{ item.title }}
                     </a>
-                    <p class="post-p">{{ item.shortDesc}}</p>
+                    <p class="post-p">{{ item.shortDesc }}</p>
                   </div>
                 </div>
               </VueSlickCarousel>
@@ -58,49 +68,6 @@
         </v-row>
       </v-container>
     </section>
-
-    <section class="section">
-      <div class="container">
-        <FeatureNews> </FeatureNews>
-      </div>
-    </section>
-
-    <section class="section">
-        <v-container>
-        <v-row>
-          <v-col cols="12" md="8">
-            <TranddingNews> </TranddingNews>
-            <div class="trending-list-view">
-              <ul class="post-list-view mt-3">
-                <!-- loop -->
-                <li  v-for="toppost in topposts" :key="toppost.id">
-                  <a href="#" class="post-list-view-link">
-                    <div class="list-img-wrap">
-                      <img
-                         :src="'http://127.0.0.1:8000/image/' + toppost.thumbnail"
-                      />
-                    </div>
-                    <div class="list-content-wrap">
-                      <div class="list-metas">
-                        <a href="" class="list-meta-category">technology</a>
-                        <a href="" class="list-meta-date">{{ moment(toppost.created_at).format("MMMM D, YYYY") }}</a>
-                      </div>
-                      <h4 class="list-content-title">
-                        {{ toppost.title }}
-                      </h4>
-                    </div>
-                  </a>
-                </li>               
-              </ul>
-            </div>
-          </v-col>
-
-          <v-col cols="12" md="4">
-            <MostView> </MostView>
-          </v-col>
-        </v-row>
-      </v-container>
-    </section>
   </div>
 </template>
 <script>
@@ -108,24 +75,14 @@ import VueSlickCarousel from "vue-slick-carousel";
 import "vue-slick-carousel/dist/vue-slick-carousel.css";
 import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
 import PostListView from "./PostListView";
-import TranddingNews from "./TranddingNews";
-import FeatureNews from "./FeatureNews";
-import TranddingListViewLeft from "./TranddingListViewLeft";
-import TranddingListViewRight from "./TranddingListViewRight";
-import MostView from "./MostView";
 import axios from "axios";
-import moment from 'moment';
+import moment from "moment";
 
 export default {
   name: "LatestNews",
   components: {
     VueSlickCarousel,
     PostListView,
-    TranddingNews,
-    FeatureNews,
-    TranddingListViewLeft,
-    TranddingListViewRight,
-    MostView,
   },
   data() {
     return {
@@ -141,12 +98,6 @@ export default {
         this.list = resp.data.data;
         console.warn(resp.data.data);
       });
-    axios
-      .post("http://127.0.0.1:8000/api/post/list", { start: 6, total: 6 })
-      .then((resp) => {
-        this.topposts = resp.data.data;
-        console.warn(resp.data.data);
-      });
   },
 };
 </script>
@@ -157,25 +108,5 @@ export default {
 }
 .latest-news-thumb {
   height: 60px;
-}
-.trending-list-view .post-list-view{
-  padding: 0;
-  margin: 0;
-}
-.trending-list-view .post-list-view li{
-  width: 100%;
-  border-color: #dcdedf;
-}
-@media (min-width:768px){
-  .trending-list-view .post-list-view li{
-    width: calc(50% - 12px);
-    box-sizing: border-box;
-  }
-  .trending-list-view .post-list-view li:nth-child(odd){
-    margin-right: 12px;
-  }
-  .trending-list-view .post-list-view li:nth-child(even){
-    margin-left: 12px;
-  }
 }
 </style>
